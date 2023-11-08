a4bef11eff38301e5579ae03a03be9ec3b0dd586
Santosl2 <mfilype2019@gmail.com>
2023-11-08 20:55:34 -0300

    feat: config express async errors and SchemaValidatorError

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,33 @@
-import express from "express";
+import "express-async-errors";
+
+import express, { NextFunction, Request, Response } from "express";
 import { WhatsappLib as whatsappLib } from "./lib/Whatsapp/Index";
 
 import http from "http";
 import { WebSocketLib } from "./lib/WebSocket";
 import { messagesRoutes } from "./routes/messages.routes";
 import "@database/index";
+import SchemaValidationError from "errors/schema-validator-error";
 
 const expressApp = express();
 expressApp.use(express.json());
 expressApp.use("/messages", messagesRoutes);
+
+expressApp.use(
+  (err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof SchemaValidationError) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 
 const app = http.createServer(expressApp);
 
